6cb84e0165fe0a584a46c4efa6ec2736072a0c80
jrambold <jrambold@gmail.com>
2018-03-26 08:41:49 -0600

    fixed pathing on spec file

--- spec/features/jobs/user_sees_one_job_spec.rb
+++ spec/features/jobs/user_sees_one_job_spec.rb
@@ -5,9 +5,9 @@ describe "User sees one job" do
     company = Company.create!(name: "ESPN")
     company.jobs.create!(title: "Developer", level_of_interest: 90, city: "Denver")
 
-    visit company_job_path(company)
+    visit company_path(company)
 
-    expect(current_path).to eq("/jobs/#{jobs.id}/jobs")
+    expect(current_path).to eq("/companies/#{company.id}/jobs")
     expect(page).to have_content("ESPN")
     expect(page).to have_content("Developer")
   end
