33147dd346225761d8fc5fa81bf57fceff11208c
adam-conway <adam.n.conway@gmail.com>
2018-03-27 11:03:01 -0600

    Removed unneccesary things

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,7 @@ Rails.application.routes.draw do
 
   resources :jobs, only: [:index, :edit, :show, :update, :destroy]
 
-  resources :categories do
-  end
+  resources :categories
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
