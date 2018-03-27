23b90bc76eee342623c468d3c8d67eb728874134
jrambold <jrambold@gmail.com>
2018-03-26 20:21:30 -0600

    fixed job create path

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -16,7 +16,7 @@ class JobsController < ApplicationController
     @job = @company.jobs.new(job_params)
     if @job.save
       flash[:success] = "You created #{@job.title} at #{@company.name}"
-      redirect_to company_job_path(@company, @job)
+      redirect_to job_path(@job)
     else
       flash[:error] = "Fail"
       render :new
