62aca973c4015458ae117e4025cae1f4d7684e64
adam-conway <adam.n.conway@gmail.com>
2018-03-28 11:50:35 -0600

    Adding finishing touches to all companies page

--- spec/features/companies/user_sees_all_companies_spec.rb
+++ spec/features/companies/user_sees_all_companies_spec.rb
@@ -9,5 +9,34 @@ describe "User sees all companies" do
 
     expect(page).to have_content("ESPN")
   end
+  describe "User clicks links" do
+    scenario "User clicks company link" do
+      company = Company.create!(name: "ESPN")
+      company_two = Company.create!(name: "Disney")
+      visit "/companies"
 
+      click_on("Disney")
+
+      expect(current_path).to eq(company_path(company_two))
+    end
+    scenario "User clicks company edit link" do
+      company = Company.create!(name: "ESPN")
+      visit "/companies"
+
+      click_on("Edit")
+
+
+      expect(current_path).to eq(edit_company_path(company))
+    end
+    scenario "User clicks company edit link" do
+      company = Company.create!(name: "ESPN")
+      visit "/companies"
+
+      click_on("Delete")
+
+      expect(current_path).to eq(companies_path)
+      expect(page).to have_content("ESPN was successfully deleted")
+      expect(Company.all.count).to eq(0)
+    end
+  end
 end
