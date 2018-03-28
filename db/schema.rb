6aa1eee123618cb630257c731f9fac3f8233fa05
adam-conway <adam.n.conway@gmail.com>
2018-03-27 19:02:53 -0600

    Adding ability to sort comments on job show page by created at date

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180327025015) do
+ActiveRecord::Schema.define(version: 20180328005101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20180327025015) do
   create_table "comments", force: :cascade do |t|
     t.string "content"
     t.bigint "job_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["job_id"], name: "index_comments_on_job_id"
   end
 
